5d4892f82bb48d8634bd99b64f14afba1e42a4ef
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 : NoxxOP <duhgfhfdghg+A1@gmail.com>
2025-10-17 07:21:15 +0530

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- XMUSIC/utils/stream/stream.py
+++ XMUSIC/utils/stream/stream.py
@@ -33,7 +33,7 @@ from XMUSIC.misc import db
 from XMUSIC.utils.database import add_active_video_chat, is_active_chat
 from XMUSIC.utils.exceptions import AssistantErr
 from XMUSIC.utils.inline import aq_markup, close_markup, stream_markup
-from XMUSIC.utils.pastebin import NandBin
+from XMUSIC.utils.pastebin import XMUSICBIN
 from XMUSIC.utils.stream.queue import put_queue, put_queue_index
 from XMUSIC.utils.thumbnails import gen_thumb
 
