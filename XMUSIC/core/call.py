e840a58ace12ac62b1b535970e0973c47fe0a926
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 : NoxxOP <duhgfhfdghg+A1@gmail.com>
2025-10-17 07:10:51 +0530

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- XMUSIC/core/call.py
+++ XMUSIC/core/call.py
@@ -57,7 +57,7 @@ from XMUSIC.utils.exceptions import AssistantErr
 from XMUSIC.utils.formatters import check_duration, seconds_to_min, speed_converter
 from XMUSIC.utils.inline.play import stream_markup
 from XMUSIC.utils.stream.autoclear import auto_clean
-from XMUSIC.utils.thumbnails import gen_thumb
+from XMUSIC.utils.thumbnails import get_thumb
 from strings import get_string
 
 autoend = {}
@@ -413,7 +413,7 @@ class Call(PyTgCalls):
                         original_chat_id,
                         text=_["call_6"],
                     )
-                img = await gen_thumb(videoid)
+                img = await get_thumb(videoid)
                 button = stream_markup(_, chat_id)
                 run = await app.send_photo(
                     chat_id=original_chat_id,
@@ -459,7 +459,7 @@ class Call(PyTgCalls):
                         original_chat_id,
                         text=_["call_6"],
                     )
-                img = await gen_thumb(videoid)
+                img = await get_thumb(videoid)
                 button = stream_markup(_, chat_id)
                 await mystic.delete()
                 run = await app.send_photo(
@@ -547,7 +547,7 @@ class Call(PyTgCalls):
                     db[chat_id][0]["mystic"] = run
                     db[chat_id][0]["markup"] = "tg"
                 else:
-                    img = await gen_thumb(videoid)
+                    img = await get_thumb(videoid)
                     button = stream_markup(_, chat_id)
                     run = await app.send_photo(
                         chat_id=original_chat_id,
