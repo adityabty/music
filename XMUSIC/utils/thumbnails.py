b68a071a1a13de9a869e7e648da03625e96a4388
Xmusic <satyabhamabehera5931@gmail.com>
2025-12-04 21:28:09 +0530

    Update thumbnails.py

--- XMUSIC/utils/thumbnails.py
+++ XMUSIC/utils/thumbnails.py
@@ -111,7 +111,7 @@ async def get_thumb(videoid: str) -> str:
     draw.text((BAR_X + BAR_TOTAL_LEN - (90 if is_live else 60), BAR_Y + 15), end_text, fill="red" if is_live else "black", font=regular_font)
 
     # Icons
-    icons_path = "XMUSIC/assets/thumb/play_icons.png"
+    icons_path = "XMUSIC/assets/thumb/IMG_20251204_211306_982.jpg"
     if os.path.isfile(icons_path):
         ic = Image.open(icons_path).resize((ICONS_W, ICONS_H)).convert("RGBA")
         r, g, b, a = ic.split()
