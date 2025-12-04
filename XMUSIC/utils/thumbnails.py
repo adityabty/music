645a938d56b6fdff30da921166224b4b6faf5a87
Xmusic <satyabhamabehera5931@gmail.com>
2025-12-04 20:45:15 +0530

    Update thumbnails.py

--- XMUSIC/utils/thumbnails.py
+++ XMUSIC/utils/thumbnails.py
@@ -1,11 +1,12 @@
-import random
-import logging
 import os
 import re
 import aiofiles
 import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
-from py_yt import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
+from config import YOUTUBE_IMG_URL
+from XMUSIC.core.dir import CACHE_DIR 
+
 
 logging.basicConfig(level=logging.INFO)
 
