e93cfbb794c8237d07daa0ede0d439c9c54c9171
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 : NoxxOP <duhgfhfdghg+A1@gmail.com>
2025-12-27 20:20:49 +0530

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- XMUSIC/plugins/misc/autoleave.py
+++ XMUSIC/plugins/misc/autoleave.py
@@ -1,55 +1,67 @@
+
 import asyncio
 from datetime import datetime
-
+from pyrogram import filters
+from pyrogram.types import Message
 from pyrogram.enums import ChatType
 
 import config
 from XMUSIC import app
+from XMUSIC.misc import SUDOERS
 from XMUSIC.core.call import JARVIS, autoend
 from XMUSIC.utils.database import get_client, is_active_chat, is_autoend
 
+AUTO_LEAVE = False
+
+
+@app.on_message(filters.command("autoleave") & SUDOERS)
+async def _auto_leave(_, message: Message):
+    global AUTO_LEAVE
+    if len(message.command) != 2:
+        return await message.reply_text("<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoleave [enable | disable]")
+    state = message.command[1].lower()
+    if state not in ["enable", "disable"]:
+        return await message.reply_text("<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoleave [enable | disable]")
+    AUTO_LEAVE = True if state == "enable" else False
+    await message.reply_text(f"Auto leave {'enabled' if AUTO_LEAVE else 'disabled'}.")
+
 
 async def auto_leave():
-    if config.AUTO_LEAVING_ASSISTANT:
-        while not await asyncio.sleep(config.AUTO_LEAVE_ASSISTANT_TIME):
-            from XMUSIC.core.userbot import assistants
-
-            for num in assistants:
-                client = await get_client(num)
-                left = 0
-                try:
-                    async for i in client.get_dialogs():
-                        if i.chat.type in [
-                            ChatType.SUPERGROUP,
-                            ChatType.GROUP,
-                            ChatType.CHANNEL,
-                        ]:
-                            if (
-                                i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1002077986660
-                                and i.chat.id != -1002166290494
-                            ):
-                                if left == 20:
-                                    continue
-                                if not await is_active_chat(i.chat.id):
-                                    try:
-                                        await client.leave_chat(i.chat.id)
-                                        left += 1
-                                    except:
-                                        continue
-                except:
-                    pass
+    while True:
+        await asyncio.sleep(14400)
+        if not AUTO_LEAVE:
+            continue
+        from XMUSIC.core.userbot import assistants
+        for num in assistants:
+            client = await get_client(num)
+            left = 0
+            try:
+                async for i in client.get_dialogs():
+                    if i.chat.type in [ChatType.SUPERGROUP, ChatType.GROUP, ChatType.CHANNEL]:
+                        if i.chat.id == config.LOG_GROUP_ID:
+                            continue
+                        if left >= 10:
+                            break
+                        if not await is_active_chat(i.chat.id):
+                            try:
+                                await client.leave_chat(i.chat.id)
+                                left += 1
+                                await asyncio.sleep(5)
+                            except:
+                                continue
+            except:
+                pass
 
 
 asyncio.create_task(auto_leave())
 
 
 async def auto_end():
-    while not await asyncio.sleep(5):
-        ender = await is_autoend()
-        if not ender:
+    while True:
+        await asyncio.sleep(5)
+        if not await is_autoend():
             continue
-        for chat_id in autoend:
+        for chat_id in list(autoend.keys()):
             timer = autoend.get(chat_id)
             if not timer:
                 continue
@@ -61,14 +73,14 @@ async def auto_end():
                 try:
                     await JARVIS.stop_stream(chat_id)
                 except:
-                    continue
+                    pass
                 try:
                     await app.send_message(
                         chat_id,
-                        "» ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+                        "» ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ."
                     )
                 except:
-                    continue
+                    pass
 
 
 asyncio.create_task(auto_end())
